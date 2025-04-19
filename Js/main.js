fa5eea3d9a3681082589271bfcdfc9c3fe4fc77a
Aryan Hardik Dani <daniaryan212@gmail.com>
2025-04-19 18:04:47 +0530

    feat: Implement debounce function and optimize service worker registration error handling

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,20 @@
+// Debounce function definition
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
 const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 document.documentElement.style.setProperty(
   "--scrollbar-width",
